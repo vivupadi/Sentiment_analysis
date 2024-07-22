834e2b6e0d6ce7c095016e90fac426a715e23cce
vivupadi <vivupadi@gmail.com>
2024-07-23 00:04:13 +0200

    GUI_set_TRain_pending

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-
-
 from train import *
+
 
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit,QPushButton, QListWidget, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit,QPushButton, QListWidget, QFileDialog, QComboBox, QLabel
 from PyQt5.QtGui import QPalette, QBrush, QPixmap
+from PyQt5.QtCore import Qt
+
+from io import BytesIO
+
+from wordcloud import WordCloud
+
+from sklearn.linear_model import LogisticRegression
+
+
+import matplotlib.pyplot as plt
 
 class Sentiment_Analysis(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sentiment Analysis")
-        self.setGeometry(200, 200,500, 500)
+        self.setGeometry(200, 200, 800, 800)
         self.initUI()
     
     def initUI(self):
@@ -22,13 +31,59 @@ class Sentiment_Analysis(QMainWindow):
 
         self.load_button = QPushButton('Select File',self)
         self.load_button.clicked.connect(self.load_file)
-        layout.addWidget(self.load_button)
+        #self.load_button.setAlignment(Qt.AlignCenter)
+        self.load_button.setFixedSize(300, 30)
+        layout.addWidget(self.load_button, alignment=Qt.AlignCenter)
+
+        self.Load_status = QLabel()
+        self.Load_status.setText('Select File')
+        self.Load_status.setAlignment(Qt.AlignCenter)
+        self.Load_status.setFixedSize(300,30)
+        layout.addWidget(self.Load_status, alignment=Qt.AlignCenter)
 
         #display data
 
         self.preprocess_button = QPushButton('Preprocess data',self)
         self.preprocess_button.clicked.connect(self.prepare_data)
-        layout.addWidget(self.preprocess_button)
+        #self.preprocess_button.setAlignment(Qt.AlignCenter)
+        self.preprocess_button.setFixedSize(300,30)
+        layout.addWidget(self.preprocess_button, alignment=Qt.AlignCenter)
+
+        self.preprocess_status = QLabel()
+        #self.preprocess_status.setText('Preprocessing in Progress...')
+        self.preprocess_status.setAlignment(Qt.AlignCenter)
+        self.preprocess_status.setFixedSize(300,30)
+        layout.addWidget(self.preprocess_status, alignment=Qt.AlignCenter)
+        
+        #display Wordmap
+        self.wordmap_button = QPushButton('Show Word Map', self)
+        self.wordmap_button.clicked.connect(self.show_wordmap)
+        self.wordmap_button.setFixedSize(300,30)
+        layout.addWidget(self.wordmap_button, alignment=Qt.AlignCenter)
+
+        #select sentiment for the wordmap
+        self.Senti_wordmap = QComboBox(self)
+        self.Senti_wordmap.addItems(["Positive", "Negative", "Neutral"])
+        self.Senti_wordmap.setFixedSize(200,30)
+        layout.addWidget(self.Senti_wordmap)
+
+        self.wordmap_label = QLabel(self)
+        self.wordmap_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.wordmap_label)
+
+        self.vector_button = QPushButton('Vectorize Data')
+        self.vector_button.clicked.connect(self.vectorize)
+        self.vector_button.setFixedSize(200,30)
+        layout.addWidget(self.vector_button)
+
+        self.train_button = QPushButton('Train model')
+        self.train_button.clicked.connect(self.training)
+        self.train_button.setFixedSize(200, 30)
+        layout.addWidget(self.train_button)
+
+        self.accuracy = QLabel(self)
+        self.accuracy.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.accuracy)
 
         central_widget.setLayout(layout)
         
@@ -38,19 +93,54 @@ class Sentiment_Analysis(QMainWindow):
         file_path, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*);;Text Files (*.txt)", options=options)
         if file_path:
             self.data = load_data(file_path)
-            #self.df = preprocess(self.data)
-            #print(self.df)
-            #return df
+            self.Load_status.setText('File Selected')
         
     def prepare_data(self):
+        self.preprocess_status.setText('Preprocessing in Progress....')
         self.df = preprocess(self.data)
+        self.preprocess_status.setText('Preprocessing Done')
         print(self.df)
 
     def show_wordmap(self):
-        #show wordmap
+        senti = self.Senti_wordmap.currentText()
+        df_sentiment = self.get_text_data(senti)
 
+        text_data = ' '.join(df_sentiment['Text'])
+        font_path = r'C:/Windows/Fonts/arial.ttf'
+        
+        word_cloud = WordCloud(width = 600, height = 400,background_color = 'black', min_font_size=6, font_path= font_path).generate(text_data)
+        plt.figure(figsize = (8,5))
+        plt.imshow(word_cloud)
+        plt.axis('off')
+        buf = BytesIO()
+        plt.savefig(buf, format='png')
+        plt.close()
+        buf.seek(0)
 
-    #def load_model():
+        pixmap = QPixmap()
+        pixmap.loadFromData(buf.getvalue())
+        self.wordmap_label.setPixmap(pixmap)
+    
+    def get_text_data(self, selected_senti):
+        df = self.df
+        if selected_senti == 'Positive':
+            return df[df['Sentiment'] == 'Positive']
+        elif selected_senti == 'Negative':
+            return df[df['Sentiment'] == 'Negative']
+        elif selected_senti == 'Neutral':
+            return df[df['Sentiment'] == 'Neutral']
+        else:
+            print('Select the type of sentiment to plot Wordcloud')
+    
+
+    def vectorize(self):
+        self.df_vector = vectorize_data(self.df)
+        print('Done Vectorization')
+
+    def training(self):
+        model = LogisticRegression(C=1, solver = 'liblinear',max_iter=50)
+        self.acc, _ = train_model(model, self.df_vector)
+        self.accuracy.setText('Accuracy_Score:', self.acc)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- train.py
+++ train.py
@@ -10,31 +10,67 @@ from nltk.corpus import stopwords
 #nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
 
 def load_data(file_path):
     data = pd.read_csv(file_path, header = 0, names=["ID","Entity","Sentiment","Text"])
+    data = data[:100]
     return data
 
 def preprocess(df):
     df['Text'] = df['Text'].str.lower()        # convert ot lowercase
     df['Text'] = [str(data) for data in df.Text]            #convert to string
-    df['Text'] = df['Text'].apply(lambda x : re.sub('[^A-Za-z0-9 ]+', ' ', x))  #remove special characters
+    df['Text'] = df['Text'].apply(lambda x : re.sub('[^A-Za-z0-9 ]+', ' ', x)) #remove special characters
     df['Text'] = df['Text'].apply(lambda x : text_clean(x))
     return df
 
 def text_clean(text):
     text = nltk.word_tokenize(text)
-    stopwords_list = stopwords.words('english')
+    stopwords_list = stopwords.words('english') #Remove stopword
     lemma = WordNetLemmatizer()
     words = [lemma.lemmatize(word) for word in text if word not in stopwords_list]
     summing = ' '.join(words)
     return summing
 
-#file_path = "C:\\Users\\Vivupadi\\Desktop\\Sentiment Analysis\\data\\twitter_training\\twitter_training.csv"
 
-#data = load_data(file_path)
-#df = preprocess(data)
-#print(df)
+def vectorize_data(df):
+    stop_words = set(stopwords.words('english'))
+    bow_counts = CountVectorizer(
+    tokenizer=word_tokenize,
+    stop_words=stop_words, #English Stopwords
+    ngram_range=(1, 1) #analysis of one word
+    )
+    df['Sentiment'] = df['Sentiment'].replace({'Positive': 1, 'Negative' : 0, 'Neutral': 2})
+    df['Text'] = bow_counts.fit_transform(df['Text'])
+    print(df)
+    return df
+
+
+#def load_model()
+
+def train_model(model, data):
+    X = data[['Text']]
+    y = data[['Sentiment']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle =True)
+    X_train = np.array(X_train, dtype=int)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    print(X_train)
+    print(y_train)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model, accuracy
+
+
 
 """
 def preprocess(data):
