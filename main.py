7fdba994ec85e34d8f148e6dd46557f412b4efe9
vivupadi <vivupadi@gmail.com>
2024-08-15 01:33:21 +0200

    QVLayout and QHLayout

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from predict import *
 
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit,QPushButton, QListWidget, QFileDialog, QComboBox, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout,QHBoxLayout, QWidget, QLineEdit,QPushButton, QListWidget, QFileDialog, QComboBox, QLabel
 from PyQt5.QtGui import QPalette, QBrush, QPixmap
 from PyQt5.QtCore import Qt
 
@@ -25,96 +25,120 @@ class Sentiment_Analysis(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        layout = QVBoxLayout()
+        layout1 = QVBoxLayout()
+        layout2 = QHBoxLayout()
+        layout3 = QHBoxLayout()
+        layout4 = QHBoxLayout()
 
         self.load_button = QPushButton('Select File',self)
         self.load_button.clicked.connect(self.load_file)
-        self.load_button.setFixedSize(200, 50)
-        layout.addWidget(self.load_button, alignment=Qt.AlignCenter)
+        self.load_button.setFixedSize(100, 30)
+        layout1.addWidget(self.load_button, alignment=Qt.AlignCenter)
 
         self.Load_status = QLabel()
         self.Load_status.setText('Select File')
         self.Load_status.setAlignment(Qt.AlignCenter)
-        self.Load_status.setFixedSize(200,30)
-        layout.addWidget(self.Load_status, alignment=Qt.AlignCenter)
+        self.Load_status.setFixedSize(100,20)
+        layout1.addWidget(self.Load_status, alignment=Qt.AlignCenter)
 
         #display data
 
         self.preprocess_button = QPushButton('Preprocess data',self)
         self.preprocess_button.clicked.connect(self.prepare_data)
         self.preprocess_button.setFixedSize(200,30)
-        layout.addWidget(self.preprocess_button, alignment=Qt.AlignCenter)
+        layout1.addWidget(self.preprocess_button, alignment=Qt.AlignCenter)
 
         self.preprocess_status = QLabel()
         self.preprocess_status.setAlignment(Qt.AlignCenter)
-        self.preprocess_status.setFixedSize(200,30)
-        layout.addWidget(self.preprocess_status, alignment=Qt.AlignCenter)
+        self.preprocess_status.setFixedSize(200,20)
+        layout1.addWidget(self.preprocess_status, alignment=Qt.AlignCenter)
+
+        #Display Sentiment selection option
+        self.sentiment_text = QLabel()
+        #self.sentiment_text.setAlignment(Qt.AlignCenter)
+        self.sentiment_text.setText('Select the Sentiment to show its Word Cloud:')
+        layout2.addWidget(self.sentiment_text)
+
+        #select sentiment for the wordmap
+        self.Senti_wordmap = QComboBox(self)
+        self.Senti_wordmap.addItems(["Positive", "Negative", "Neutral", "Irrelevant"])
+        #self.Senti_wordmap.setFixedSize(200,30)
+        layout2.addWidget(self.Senti_wordmap)
+        
+        layout1.addLayout(layout2)
         
         #display Wordmap
         self.wordmap_button = QPushButton('Show Word Map', self)
         self.wordmap_button.clicked.connect(self.show_wordmap)
         self.wordmap_button.setFixedSize(150,50)
-        layout.addWidget(self.wordmap_button, alignment=Qt.AlignCenter)
-
-        #select sentiment for the wordmap
-        self.Senti_wordmap = QComboBox(self)
-        self.Senti_wordmap.addItems(["Positive", "Negative", "Neutral", "Irrelevant"])
-        self.Senti_wordmap.setFixedSize(200,30)
-        layout.addWidget(self.Senti_wordmap)
+        layout1.addWidget(self.wordmap_button, alignment=Qt.AlignCenter)
 
         #Display WOrdCloud
         self.wordmap_label = QLabel(self)
         self.wordmap_label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.wordmap_label)
+        layout1.addWidget(self.wordmap_label)
+
+        #Display vector selection option
+        self.vect_text = QLabel()
+        self.vect_text.setText('Select the type of vectorizer:')
+        layout3.addWidget(self.vect_text)
 
         #Select the vectorizer
         self.select_vectorizer = QComboBox(self)
         self.select_vectorizer.addItems(["Bow", "TF_IDF"])
         self.select_vectorizer.setFixedSize(200,30)
-        layout.addWidget(self.select_vectorizer, alignment=Qt.AlignCenter)
+        layout3.addWidget(self.select_vectorizer)
+
+        layout1.addLayout(layout3)
 
         #Button to vectorize Text data
         self.vector_button = QPushButton('Vectorize Data')
         self.vector_button.clicked.connect(self.vectorize)
         self.vector_button.setFixedSize(200,30)
-        layout.addWidget(self.vector_button, alignment=Qt.AlignCenter)
+        layout1.addWidget(self.vector_button, alignment=Qt.AlignCenter)
+
+        #Display vector selection option
+        self.model_text = QLabel()
+        self.model_text.setText('Select model to be trained:')
+        layout4.addWidget(self.model_text)
 
         #Select model
         self.select_model = QComboBox(self)
         self.select_model.addItems(["LogisticRegression", "XGBoost", "Random Forest"])
         self.select_model.setFixedSize(200,30)
-        layout.addWidget(self.select_model, alignment=Qt.AlignCenter)
+        layout4.addWidget(self.select_model)
 
+        layout1.addLayout(layout4)
 
         #Button to Train model
         self.train_button = QPushButton('Train model')
         self.train_button.clicked.connect(self.training)
         self.train_button.setFixedSize(200, 30)
-        layout.addWidget(self.train_button)
+        layout1.addWidget(self.train_button, alignment= Qt.AlignCenter)
 
         #Dipslay accuracy
         self.accuracy = QLabel(self)
         self.accuracy.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.accuracy)
+        layout1.addWidget(self.accuracy)
 
         #Field to enter a User defined New tweet
         self.prompt = QLineEdit(self)
         self.prompt.setPlaceholderText("Enter a Tweet")
-        layout.addWidget(self.prompt)
+        layout1.addWidget(self.prompt)
 
         #predict button
         self.predict_button = QPushButton('Predict the text Sentiment')
         self.predict_button.clicked.connect(self.predict)
         self.predict_button.setFixedSize(200, 30)
-        layout.addWidget(self.predict_button)
+        layout1.addWidget(self.predict_button)
 
 
         #Display the sentiment of the entered Text 
         self.display_sentiment = QLabel(self)
         self.display_sentiment.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.display_sentiment)
+        layout1.addWidget(self.display_sentiment)
 
-        central_widget.setLayout(layout)
+        central_widget.setLayout(layout1)
         
 
     def load_file(self):
